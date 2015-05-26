0ae3dcc91f94f2f9775c1d4d8ab2d5d88f38bc6d
wittrup <wittrup@gmail.com>
2015-05-26 20:57:03 +0200

    Auto save pictures appearing in clipboard to selected folder.
    Delphi VCL tool to automatically save pictures to selected folder when
    appearing in clipboard (Printscreen, Ctrl+C, programs with Copy to
    clipboard buttons). Saving format PNG. Filename is set to timestamp,
    format'yyyy-mm-dd hhnnss'. Tool minimize to tray. CoPiSave avoids
    duplicates by checking if the data is similar to the what is already in
    clipboard (Snipping Tool updates clipboard on close).

---
+++ CoPiSave.dpr
@@ -0,0 +1,25 @@
+program CoPiSave;
+
+{ Reduce EXE size by disabling as much of RTTI as possible (delphi 2009/2010) }
+{$IF CompilerVersion >= 21.0}
+  {$WEAKLINKRTTI ON}
+  {$RTTI EXPLICIT METHODS([]) PROPERTIES([]) FIELDS([])}
+{$ENDIF}
+
+// This will strip the relocation information which is not needed in a .exe.
+// Don't add this to a DLL or package!
+{$SetPEFlags 1}
+
+uses
+  Vcl.Forms,
+  main in 'main.pas' {Form1};
+
+{$R *.res}
+
+begin
+  System.ReportMemoryLeaksOnShutdown := True;
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
